847e3c3d943ca7882032437d87d29c1bfb2b2007
1MAN1WHA1 <157196605+1MAN1WHA1@users.noreply.github.com>
2026-04-14 17:50:25 +0700

    Update view_lesson.php

--- m98312qr.beget.tech/public_html/view_lesson.php
+++ m98312qr.beget.tech/public_html/view_lesson.php
@@ -60,7 +60,6 @@ $hwNone    = ($hwStatus === 'none');
 <head>
     <meta charset="utf-8">
     <title><?= e($lesson['title'] ?? 'Урок') ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 <div class="container mt-4">
