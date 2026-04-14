4075308b4afea5145c260259ddf5c926908dcba0
1MAN1WHA1 <157196605+1MAN1WHA1@users.noreply.github.com>
2026-04-14 17:48:23 +0700

    Update admin_orders.php

--- m98312qr.beget.tech/public_html/admin_orders.php
+++ m98312qr.beget.tech/public_html/admin_orders.php
@@ -21,7 +21,6 @@ $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <title>Заказы</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="p-4">
     <h1>Все заказы</h1>
