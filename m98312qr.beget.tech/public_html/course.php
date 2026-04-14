67c95e5d5145afd529f760458ead1ae8f5ba8859
1MAN1WHA1 <157196605+1MAN1WHA1@users.noreply.github.com>
2026-04-14 17:49:58 +0700

    Update course.php

--- m98312qr.beget.tech/public_html/course.php
+++ m98312qr.beget.tech/public_html/course.php
@@ -73,7 +73,6 @@ if ($has_access) {
 <head>
   <meta charset="utf-8">
   <title><?= htmlspecialchars($course['title']) ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 <div class="container mt-4">
