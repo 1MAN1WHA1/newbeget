16d56b7a3d43edceacf764c7592e1fad3f5ca7b3
1MAN1WHA1 <157196605+1MAN1WHA1@users.noreply.github.com>
2026-04-14 17:55:48 +0700

    Update header.php

--- m98312qr.beget.tech/public_html/site-core/templates/layout/header.php
+++ m98312qr.beget.tech/public_html/site-core/templates/layout/header.php
@@ -7,7 +7,6 @@ $pageTitle = $pageTitle ?? 'Главная';
 <head>
     <meta charset="UTF-8">
     <title><?= e($pageTitle) ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
