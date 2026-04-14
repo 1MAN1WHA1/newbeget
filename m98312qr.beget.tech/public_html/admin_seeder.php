335292c422ae697960779fad835074c37a94b4f7
1MAN1WHA1 <157196605+1MAN1WHA1@users.noreply.github.com>
2026-04-14 17:48:41 +0700

    Update admin_seeder.php

--- m98312qr.beget.tech/public_html/admin_seeder.php
+++ m98312qr.beget.tech/public_html/admin_seeder.php
@@ -93,7 +93,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Seeder + CSV</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light p-4">
 
