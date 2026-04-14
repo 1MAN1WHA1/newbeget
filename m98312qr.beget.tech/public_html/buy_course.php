86ecaafd58963c69391635a7bf0ea90b779e77cd
1MAN1WHA1 <157196605+1MAN1WHA1@users.noreply.github.com>
2026-04-14 17:49:01 +0700

    Update buy_course.php

--- m98312qr.beget.tech/public_html/buy_course.php
+++ m98312qr.beget.tech/public_html/buy_course.php
@@ -31,7 +31,6 @@ if ($paid->fetchColumn()) {
 <head>
     <meta charset="utf-8">
     <title>Оплата курса</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 <div class="container mt-4">
